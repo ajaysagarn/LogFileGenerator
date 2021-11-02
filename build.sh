dff9ce876169cea3b146eabbd83f92b7943bdc92
ajaysagarn <ajaysagarn@gmail.com>
2021-11-02 02:19:19 -0500

    update build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 cd /var/scala/LogFileGenerator #navigate to the log generator folder
 rm -r /var/scala/LogFileGenerator/log/ #remove previous logs
-sbt run # run the project to generate the logs
-aws s3 rm s3://ajsa-aws-emr/input --recursive #remove previous logs from the s3 bucket
-aws s3 mv /var/scala/LogFileGenerator/log/ s3://ajsa-aws-emr/input --recursive # move the newly generated logs to s3
+/home/ec2-user/.sdkman/candidates/sbt/current/bin/sbt run # run the project to generate the logs
+/usr/bin/aws s3 rm s3://ajsa-aws-emr/input --recursive #remove previous logs from the s3 bucket
+/usr/bin/aws s3 mv /var/scala/LogFileGenerator/log/ s3://ajsa-aws-emr/input --recursive # move the newly generated logs to s3
 
